284ac683bcd8912c03f860153f79561925559c76
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-18 20:59:37 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,11 +9,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-div = num2 / num1
+  div = num2 / num1
+return = div
 end
 
 def multiplication(num1, num2)
-
+  mult = num1 * num2
+  return = mult
 end
 
 def modulo(num1, num2)
